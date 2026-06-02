0826b8b418533868f09e24d6ed71bb58a30c354e
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2026-06-02 18:00:01 -0300

    Refactor RoadmapPage: add useDispatch hook for future state management

--- src/pages/RoadmapPage.jsx
+++ src/pages/RoadmapPage.jsx
@@ -1,9 +1,12 @@
+// @ts-nocheck
 import React from "react";
 import RoadmapView from "../components/RoadmapView";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function RoadmapPage() {
   const suggestions = useSelector((s) => s.feedback.suggestions);
+  const dispatch = useDispatch();
+  
 
   return (
     <div className="max-w-6xl mx-auto">
